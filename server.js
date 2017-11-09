eebf724cad990d16f1bbd9b75f7615702f39a2e9
Mark Kopenga <mkopenga@gmail.com>
2017-11-09 22:53:12 +0100

    added discord bot and config file

--- server.js
+++ server.js
@@ -1,79 +1,71 @@
-'use strict';
-
-const express = require('express')
-const bodyParser = require('body-parser')
 const fs = require('fs-extra')
-const shell = require('shelljs')
-const watch = require('node-watch')
-const sass = require('npm-sass')
-const fetch = require('node-fetch')
-const app = express()
+var config = fs.readJsonSync('./config.json')
+if (config.DiscordToken != " --- BOT TOKEN HERE --- ") {
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const shell = require('shelljs')
+  const watch = require('node-watch')
+  const sass = require('npm-sass')
+  const fetch = require('node-fetch')
+  const Discord = require("discord.js")
+  const client = new Discord.Client()
+  const app = express()
 
-app.use(express.static('./www/'))
-app.use(bodyParser.json(true));
-app.use(bodyParser.urlencoded({extended: true}));
+  app.use(express.static('./www/'))
+  app.use(bodyParser.json(true));
+  app.use(bodyParser.urlencoded({extended: true}));
 
-var messages = []
+  var messages = []
 
-app.get("/stopscript", function(req,res) {
-  process.exit()
-  setTimeout(function () {
-    res.send('OK');
-  }, 10);
-})
-
-const port = 7777;
-
-fetch('http://127.0.0.1:' + port + '/')
-.then(function(res) {
-  if (res.ok) {
-    fetch('http://127.0.0.1:' + port + '/stopscript')
-    .then(function(res) {
-      return res.text();
-    }).then(function(body) {
-
-    }).catch(function(err) {
-      console.log(' ');
-      console.log('---------------------------------');
-      console.log('Port: ' + port + ' already in use!');
-      console.log('Try to start the webserver again');
-      console.log('---------------------------------');
-      process.exit()
-    });
+  // a route to stop the script
+  app.get("/stopscript", function(req,res) {
+    process.exit()
     setTimeout(function () {
-      console.log(' ');
-      console.log('---------------------------------');
-      console.log('Port: ' + port + ' already in use!');
-      console.log('Try to start the webserver again');
-      console.log('---------------------------------');
-      process.exit()
-    }, 1000);
-  } else {
+      res.send('OK');
+    }, 10);
+  })
+
+  const port = 7777;
+
+  // test if there is already a server running if so stop it and ask the user to run the script again
+  fetch('http://127.0.0.1:' + port + '/')
+  .then(function(res) {
+    if (res.ok) {
+      fetch('http://127.0.0.1:' + port + '/stopscript')
+      .then(function(res) {
+        return res.text();
+      }).then(function(body) {
+
+      }).catch(function(err) {
+        console.log(' ');
+        console.log('---------------------------------');
+        console.log('Port: ' + port + ' already in use!');
+        console.log('Try to start the webserver again');
+        console.log('---------------------------------');
+        process.exit()
+      });
+      setTimeout(function () {
+        console.log(' ');
+        console.log('---------------------------------');
+        console.log('Port: ' + port + ' already in use!');
+        console.log('Try to start the webserver again');
+        console.log('---------------------------------');
+        process.exit()
+      }, 1000);
+    } else {
+      startserv()
+    }
+  }).catch(function(err) {
     startserv()
+  });
+  function startserv() {
+    app.listen(port);
+    console.log('Started server on port:' + port);
   }
-}).catch(function(err) {
-  startserv()
-});
-function startserv() {
-  app.listen(port);
-  console.log('Started server on port:' + port);
-}
 
-watch('./www/css/', { recursive: true }, function(evt, name) {
-  if (evt == 'update' && name.endsWith('.sass')) {
-    sass('./' + name, function (err, result) {
-      if (!err) {
-        console.log('compiled sass file: ' + result.stats.entry);
-        fs.writeFile(result.stats.entry.replace('.sass','.css'),result.css)
-      }
-    });
-  }
-});
-
-fs.readdir('./www/css/', function(err, items) {
-  for (var i = 0; i < items.length; i++) {
-    var name = 'www/css/' + items[i]
-    if (name.endsWith('.sass')) {
+  // sass compiler
+  watch('./www/css/', { recursive: true }, function(evt, name) {
+    if (evt == 'update' && name.endsWith('.sass')) {
       sass('./' + name, function (err, result) {
         if (!err) {
           console.log('compiled sass file: ' + result.stats.entry);
@@ -81,5 +73,33 @@ fs.readdir('./www/css/', function(err, items) {
         }
       });
     }
-  }
-});
+  });
+
+  fs.readdir('./www/css/', function(err, items) {
+    for (var i = 0; i < items.length; i++) {
+      var name = 'www/css/' + items[i]
+      if (name.endsWith('.sass')) {
+        sass('./' + name, function (err, result) {
+          if (!err) {
+            console.log('compiled sass file: ' + result.stats.entry);
+            fs.writeFile(result.stats.entry.replace('.sass','.css'),result.css)
+          }
+        });
+      }
+    }
+  });
+
+  client.on('ready', () => {
+    console.log('I am ready!');
+  });
+
+  client.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+  });
+
+  client.login(config.DiscordToken);
+} else {
+  console.log('No discord token inside config.json');
+}
